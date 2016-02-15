b224240f83f0e356a5e1985ff1ceebd83d32d00c
Enrico Sada <enrico@sada.io>
2016-02-15 18:34:03 +0100

    fix tests, because atm two dotnet build are required

--- scripts/run_tests.ps1
+++ scripts/run_tests.ps1
@@ -4,19 +4,30 @@ $repoDir = Split-Path -parent (Split-Path -parent $PSCommandPath)
 
 Push-Location
 
-# restore
-
 cd "$repoDir\test\use-dotnet-fssrgen-as-tool"
+
+# restore
 
 dotnet restore
 if (-not $?) {
 	exit 1
 }
 
+# build
+
 dotnet build
 if (-not $?) {
 	exit 1
 }
+
+# build another time, because .resx are built before precompile event
+
+dotnet build
+if (-not $?) {
+	exit 1
+}
+
+# run tests
 
 dotnet test
 if (-not $?) {
