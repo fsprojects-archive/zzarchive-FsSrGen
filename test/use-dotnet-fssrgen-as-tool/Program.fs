5d3991fd9b37cec5bd6a5b1e26ed723a1ca100a4
Enrico Sada <enrico@sada.io>
2016-02-15 16:54:39 +0100

    add test without precompile

--- test/use-dotnet-fssrgen-as-tool/Program.fs
+++ test/use-dotnet-fssrgen-as-tool/Program.fs
@@ -1,9 +1,16 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿module FsSrGenTest
 
 open System
+open System.Reflection
+open System.Resources
 
 [<EntryPoint>]
 let main argv = 
-    printfn "Hello World!"
-    printfn "%A" argv
-    0 // return an integer exit code
+
+    let msg = FSComp.SR.sayHello "World"
+    printfn "%s" msg
+
+    let errNum,errMsg = FSComp.SR.invalidName "Kilo"
+    printfn "Err %i: %s" errNum errMsg
+
+    0
